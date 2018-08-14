39fb642e8682067d88067836c2251c14cade2bfa
Markus Hintersteiner <m.hintersteiner@gmail.com>
2018-08-14 20:28:07 +0200

    Fix added license headers

--- lib/src/main/java/com/github/okdroid/checkablechipview/CheckableChipView.kt
+++ lib/src/main/java/com/github/okdroid/checkablechipview/CheckableChipView.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.okdroid.checkablechipview
 
 import android.animation.ValueAnimator
@@ -44,7 +60,7 @@ class CheckableChipView @JvmOverloads constructor(
     }
 
     /**
-     * Sets the backgroudn color when the widget is checked.
+     * Sets the background color when the widget is checked.
      */
     var checkedColor: Int = 0xffff00ff.toInt()
         set(value) {
